800cece440246d40e5f834c6f5cf59d780ada425
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-21 14:45:44 +0530

    Added:Welcome Message

---
+++ src/main/java/com/censusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,9 @@
+package com.censusanalyser;
+
+public class StateCensusAnalyser
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Census analyser problem");
+    }
+}
