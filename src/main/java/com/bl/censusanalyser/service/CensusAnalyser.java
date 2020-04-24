4771cd7bc56b01a3956e2400b2e2e01218cd6571
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-24 15:01:09 +0530

    REFACTORED: Refactor 2 code to ensure single responsibility for analyser and use delegation principle to handl csv file

--- src/main/java/com/bl/censusanalyser/service/CensusAnalyser.java
+++ src/main/java/com/bl/censusanalyser/service/CensusAnalyser.java
@@ -3,6 +3,7 @@ package com.bl.censusanalyser.service;
 import com.bl.censusanalyser.exception.CensusAnalyserException;
 import com.bl.censusanalyser.model.CSVStateCensus;
 import com.bl.censusanalyser.model.StateCode;
+import com.bl.censusanalyser.utility.OpenCSV;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -18,33 +19,27 @@ import java.util.Iterator;
     public class CensusAnalyser
     {
         public static int loadStateCensusCSVFileData(String filePath) throws CensusAnalyserException, IOException {
-            int noOfRecords = 0;
-            try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
-
-                Iterator<CSVStateCensus> csvRecords = getCSVFileIterator(reader, CSVStateCensus.class);
-                while (csvRecords.hasNext()) {
-                    noOfRecords++;
-                    csvRecords.next();
-                }
-                return noOfRecords;
-            } catch (NoSuchFileException e) {
+            try (Reader reader = Files.newBufferedReader(Paths.get(filePath)))
+            {
+                Iterator<CSVStateCensus> csvRecords = OpenCSV.getCSVFileIterator(reader,CSVStateCensus.class);
+                return OpenCSV.getCount(csvRecords);
+            }
+            catch (NoSuchFileException e)
+            {
                 throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.ENTERED_WRONG_FILE_NAME,
                         "FILE NAME IS INCORRECT");
-            } catch (RuntimeException e) {
+            }
+            catch (RuntimeException e)
+            {
                 throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.INCORRECT_DELIMITER_OR_HEADER,
                         "FILE DELIMITER OR HEADER IS INCORRECT");
             }
         }
-
         public static int loadStateCodeCSVFileData(String filePath) throws IOException, CensusAnalyserException {
-            int noOfRecords = 0;
-            try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
-                Iterator<StateCode> csvRecords = getCSVFileIterator(reader, StateCode.class);
-                while (csvRecords.hasNext()) {
-                    noOfRecords++;
-                    csvRecords.next();
-                }
-                return noOfRecords;
+            try (Reader reader = Files.newBufferedReader(Paths.get(filePath)))
+            {
+            Iterator<StateCode> csvRecords = OpenCSV.getCSVFileIterator(reader,StateCode.class);
+            return  OpenCSV.getCount(csvRecords);
             }
             catch (NoSuchFileException e){
                 throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.ENTERED_WRONG_FILE_NAME,
@@ -56,7 +51,6 @@ import java.util.Iterator;
                         "FILE DELIMITER OR HEADER IS INCORRECT");
             }
         }
-
         public static void getFileExtension(File filePath) throws CensusAnalyserException
         {
             String fileName = filePath.getName();
@@ -72,18 +66,6 @@ import java.util.Iterator;
             }
         }
 
-        private static <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyserException
-        {
-            try
-            {
-                CSVReader csvReader = new CSVReader(reader);
-                CsvToBean<E> csvToBean = new CsvToBeanBuilder<E>(csvReader).withType(csvClass).build();
-                return csvToBean.iterator();
-            }
-            catch (IllegalStateException e)
-            {
-                throw new CensusAnalyserException(CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE, e.getMessage());
-            }
-        }
+
     }
 
